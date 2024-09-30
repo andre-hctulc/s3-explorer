870ab70935b59b5b3c54ef23a8b88a69c5ec5a84
andre-hctulc <andre.tho.44@gmail.com>
2024-09-30 15:18:58 +0200

    init test setup

--- test/conn.test.ts
+++ test/conn.test.ts
@@ -9,3 +9,24 @@ const conn = new S3Connection(process.env.BUCKET_NAME || "", {
         region: process.env.AWS_REGION,
     },
 });
+
+function entries(): Promise<string[]> {
+    return [];
+}
+
+function compareEntries(entries: string[], expected: string[]) {
+    return { missing: [], extra: [], equal: true };
+}
+
+describe("S3Connection", () => {
+    it("should connect to S3", async () => {
+        const data = await conn.getText("test.txt");
+        expect(data).toBe("Hello, world!");
+    });
+
+    it("should put data to S3", async () => {
+        await conn.put("test.txt", "Hello, world!");
+        const data = await conn.getText("test.txt");
+        expect(data).toBe("Hello, world!");
+    });
+});
